44783305e57e17377551cafae8e17d984504eff4
Kenta Katsumata <kenta.katsumata@aktsk.jp>
2016-04-12 14:44:12 +0900

    #1 add translate/4 helper function

--- lib/translator/translation_helper.ex
+++ lib/translator/translation_helper.ex
@@ -1,12 +1,23 @@
 defmodule Translator.TranslationHelpers do
-  def translate(model, field, assoc \\ :translation)
+  @default_field :translation
+
+  def translate(%Ecto.Changeset{} = changeset, model, field), do: translate(changeset, model, field, @default_field)
+  def translate(%Ecto.Changeset{} = changeset, model, field, assoc) do
+    if Map.has_key?(changeset.changes, field) do
+      Map.get(changeset.changes, field)
+    else
+      translate(model, field, assoc)
+    end
+  end
+
+  def translate(model, field), do: translate(model, field, @default_field)
   def translate(nil, _field, _assoc), do: ""
   def translate(model, field, assoc) do
     translation = Map.get(model, assoc)
     if !is_nil(translation) && Ecto.assoc_loaded?(translation) && Map.has_key?(translation, field) do
-      Map.get(translation, field)
+      Map.get(translation, field) || ""
     else
-      Map.get(model, field)
+      Map.get(model, field) || ""
     end
   end
 end
